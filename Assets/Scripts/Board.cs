5527f4d6c2be0b398fc6a4fa905e25dee5a1b6d0
Nikola <nikoladamjan@live.com>
2021-04-08 00:46:57 +0200

    First commit

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Board : MonoBehaviour
+{
+
+    public GameObject cellPrefab;
+
+    [HideInInspector]
+    public Cell[,] allCells = new Cell[8, 8];
+    // Start is called before the first frame update
+    public void Create()
+    {
+        int br = 1;
+        for(int i = 0; i < 8; i++)
+        {
+            for(int j = 0; j<8; j++)
+            {
+
+                br++;
+                GameObject newCell = Instantiate(cellPrefab,transform);
+
+                RectTransform rectTransform = newCell.GetComponent<RectTransform>();
+                rectTransform.anchoredPosition = new Vector2((j * 100) + 50, (i * 100) + 50);
+
+                allCells[i,j] = newCell.GetComponent<Cell>();
+                allCells[i,j].Setup(new Vector2Int(i,j), this);
+                if (br % 2 != 0)
+                {
+                    allCells[i, j].GetComponent<Image>().color = new Color32(230, 220, 186, 255);
+                }
+            }
+            br++;
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+
+    public Board board;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        board.Create();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
